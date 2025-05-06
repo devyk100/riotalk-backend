1f0d185e7b7aa3f327c5127cdf6fe1f61ca9e24d
Yash Ramesh Kumar <devyk100@gmail.com>
2025-05-06 12:22:27 +0530

    some changes to redis recent messages

--- shared/redis/get-recent-messages.go
+++ shared/redis/get-recent-messages.go
@@ -3,15 +3,13 @@ package redis
 import (
 	"context"
 	"fmt"
-	"sort"
 )
 
 func GetRecentMessages(ctx context.Context, channelID string) ([]string, error) {
-	messages, err := RedisClient.LRange(ctx, channelID, 0, RECENT_MESSAGE_THRESHOLD).Result()
+	messages, err := RedisClient.LRange(ctx, channelID, 0, -1).Result()
 	if err != nil {
 		fmt.Println(err)
 		return nil, err
 	}
-	sort.Slice(messages, func(i, j int) bool { return i > j })
 	return messages, nil
 }
--- shared/redis/push-to-recent.go
+++ shared/redis/push-to-recent.go
@@ -8,12 +8,13 @@ import (
 var RECENT_MESSAGE_THRESHOLD = int64(25)
 
 func PushToRecentMessages(ctx context.Context, channel string, message string) {
-	err := RedisClient.LPush(ctx, channel, message).Err()
+	err := RedisClient.RPush(ctx, channel, message).Err()
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-	err = RedisClient.LTrim(ctx, channel, 0, RECENT_MESSAGE_THRESHOLD).Err()
+
+	err = RedisClient.LTrim(ctx, channel, -RECENT_MESSAGE_THRESHOLD, -1).Err()
 	if err != nil {
 		fmt.Println(err.Error())
 		return
